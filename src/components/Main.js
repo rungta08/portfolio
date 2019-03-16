382a187c1ec39364b261f877032f6b02c4a04969
Shivam Rungta <shivam08rungta@gmail.com>
2019-03-17 00:41:47 +0530

    Home Improved

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ class Main extends Component{
             <Container className='main'>
                 <Row>
                     <Col lg='7' md='7' xs='12' className='main-text'>
-                        <h2>Well! Hello I am <span>Shivam Rungta</span><br/>I am a Full - Stack Developer</h2>
+                        <h2>Well! Hello I am <span className='name'>Shivam Rungta</span><br/>I am a <span className='desig'>Full - Stack Developer</span></h2>
                     </Col>
                     <Col className='main-img' lg='5' md='5' xs='12'>
                         <img src="https://github.com/rungta08/portfolio/blob/gh-pages/assets/images/Home-Img.png?raw=true" alt='welcome' />
