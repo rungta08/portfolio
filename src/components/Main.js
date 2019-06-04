530412449474810cdbe7359cbf7659bf4f33ed2e
rungta08 <shivam08rungta@gmail.com>
2019-06-04 11:42:42 +0530

    changes image path

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ class Main extends Component{
                         <h2>Well! Hello I am <span className='name'>Shivam Rungta</span><br/>I am a <span className='desig'>Full - Stack Developer</span></h2>
                     </Col>
                     <Col className='main-img' lg='5' md='5' xs='12'>
-                        <img className='home-img' src="https://github.com/rungta08/portfolio/blob/gh-pages/assets/images/Home-Img.png?raw=true" alt='welcome' />
+                        <img className='home-img' src="https://github.com/rungta08/portfolio/blob/master/src/assets/images/Home-Img.png?raw=true" alt='welcome' />
                     </Col>
                 </Row>
             </Container>
