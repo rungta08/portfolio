1f69c3f85b2c6fd953ec215fb1038d45683ba760
rungta08 <shivam08rungta@gmail.com>
2019-06-04 11:36:26 +0530

    Updates

---
+++ precache-manifest.c08f9fce3d3038360839d1327ed238bd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "4e3224e545129f4f9d55",
+    "url": "/portfolio/static/css/main.14d0468f.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c3f1dc1132526cd9e5b4",
+    "url": "/portfolio/static/js/2.144cba5c.chunk.js"
+  },
+  {
+    "revision": "4e3224e545129f4f9d55",
+    "url": "/portfolio/static/js/main.e6ec62d6.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "c3f1dc1132526cd9e5b4",
+    "url": "/portfolio/static/css/2.ca1b21d8.chunk.css"
+  },
+  {
+    "revision": "ec98b4896897e58694af656fd29f8ba0",
+    "url": "/portfolio/index.html"
+  }
+];
